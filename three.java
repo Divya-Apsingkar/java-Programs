ee288caebf7c830a370cbc18d5f0d80ef9bed767
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:19:55 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class three
+{
+      public static void main(String args[ ])
+      {
+        short a;
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter short Number");
+       a=s.nextShort( );
+     System.out.println(a);
+      }
+}
