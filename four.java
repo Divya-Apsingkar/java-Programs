7b3a36a5ecfb4c24985bada393960b79f0bbeb7a
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:20:11 +0530

    Add files via upload

---
+++ four.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class four
+{
+    public static void main(String args[ ])
+     {
+                int b;
+                Scanner s=new Scanner(System.in);
+                System.out.println("Enter 1 Integer");
+                b=s.nextInt( );
+               System.out.println(b);
+     }
+}
