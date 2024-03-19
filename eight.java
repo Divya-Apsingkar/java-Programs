abb21b5eb31715374417a0a35fc853d81ade8d42
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:21:45 +0530

    Add files via upload

---
+++ eight.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class eight
+{
+   public static void main(String args[ ])
+      {
+       int a;
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter Number for Printing name");
+        a=s.nextInt( );
+        for(int i=0;i<a;i++)
+{
+     System.out.println("Divya Avinash Apsingkar");
+} 
+      }
+}
