bf14b819ac910147b9870621189c823f5eeb70f0
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:20:31 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class five
+{
+       public static void main(String args[ ])
+        {
+           long c;
+           Scanner s=new Scanner(System.in);
+           System.out.println("Enter long Number");
+           c=s.nextLong();
+          System.out.println(c);
+        }
+}
