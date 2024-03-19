f3172f09c465a24d9e7113ed9736a374bdf32de2
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:19:32 +0530

    Add files via upload

---
+++ two.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class two
+{
+    public static void main(String args[ ])
+          {
+         byte d;
+        Scanner s=new Scanner(System.in);
+       System.out.println("Enter 1 Byte :");
+    d=s.nextByte( );
+     System.out.println(d);
+          }
+}
