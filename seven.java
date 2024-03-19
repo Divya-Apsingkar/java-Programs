9b5d67f760c06c4e960943a52d08ced26ebbd31b
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:21:07 +0530

    Add files via upload

---
+++ seven.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class seven
+{
+     public static void main(String args[ ])
+     {
+        double a;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter 1 double ");
+         a=s.nextDouble( );
+         System.out.println(a);
+     }
+}
