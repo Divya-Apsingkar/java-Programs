30847b2aa6b4533aa83f88d03c90f7d5ed352ddb
Divya Avinash Apsingkar <117099978+Divya-Apsingkar@users.noreply.github.com>
2024-03-19 15:20:48 +0530

    Add files via upload

---
+++ six.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class six
+{
+    public static void main(String args[ ])
+     {
+      float a;
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter 1  float :");
+      a=s.nextFloat( );
+      System.out.println(a);
+     }
+}
